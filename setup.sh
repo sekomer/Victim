14c212a96780f24c87ea8ef44319252790732513
Alperen Serkan Aksöz <61659936+Sekomer@users.noreply.github.com>
2022-07-28 18:49:25 +0300

    killing the dolly

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@
 # install
 echo '[begin]'
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-git clone https://github.com/Sekomer/Victim
-cd Victim
+source ~/.bashrc
 cabal install -O2 --overwrite-policy=always
 
 # test
